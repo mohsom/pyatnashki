672f89a457c140d24ea1bc6690462760e6c04749
mohsom <volodya.skalskyi@gmail.com>
2017-04-22 20:23:28 +0300

    Working on tiles shuffling

--- src/Game/App.js
+++ src/Game/App.js
@@ -20,49 +20,64 @@ class App extends Component {
                 y: 4
             }
         };
-        this.moveTile = this.moveTile.bind(this);
+        this.keyPress = this.keyPress.bind(this);
     };
 
 
     componentWillMount() {
-        document.addEventListener('keydown', this.moveTile);
+        document.addEventListener('keydown', this.keyPress);
     };
 
     componentDidMount() {
-       this.fillGrid();
+        this.fillGrid();
+
     };
 
     fillGrid() {
-        console.log(this.getRandomPosition());
-        let  grid =[], val = 1;
-        for(let i=0; i<this.state.size; i++) {
+        let grid = [], val = 1;
+        for (let i = 0; i < this.state.size; i++) {
             grid[i] = new Array(this.state.size);
-            for(let j=0; j<this.state.size; j++) {
+            for (let j = 0; j < this.state.size; j++) {
                 grid[i][j] = val;
                 val++;
             }
         }
-        grid[this.state.size-1][this.state.size-1] = null;
+        grid[this.state.size - 1][this.state.size - 1] = null;
         this.setState({
             grid
         });
     };
 
-    getRandomPosition() {
-        return {
-            x: Math.floor(Math.random() * (this.state.size-1)),
-            y: Math.floor(Math.random() * (this.state.size-1))
+    keyPress(e) {
+        switch (e.keyCode) {
+            case arrows.up: {
+                this.moveTile('up');
+                break;
+            }
+            case arrows.down: {
+                this.moveTile('down');
+                break;
+            }
+
+            case arrows.left: {
+                this.moveTile('left');
+                break;
+            }
+
+            case arrows.right: {
+                this.moveTile('right');
+                break;
+            }
         }
     };
 
-    moveTile(e) {
-        let key  = e.keyCode;
+    moveTile(key) {
         let x = this.state.emptyPos.x;
         let y = this.state.emptyPos.y;
         let grid = this.state.grid;
         switch (key) {
-            case arrows.up: {
-                if(x>0) {
+            case 'up': {
+                if (x > 0) {
                     grid[x][y] = grid[x - 1][y];
                     grid[x - 1][y] = null;
                     x--;
@@ -71,35 +86,34 @@ class App extends Component {
                 break;
             }
 
-            case arrows.down: {
-                if(x<this.state.size - 1) {
-                    grid[x][y] = grid[x+1][y];
-                    grid[x+1][y] = null;
+            case 'down': {
+                if (x < this.state.size - 1) {
+                    grid[x][y] = grid[x + 1][y];
+                    grid[x + 1][y] = null;
                     x++;
                 }
                 console.log('down!');
                 break;
             }
-            case arrows.left: {
-                if(y>0) {
-                    grid[x][y] = grid[x][y-1];
-                    grid[x][y-1] = null;
+            case 'left': {
+                if (y > 0) {
+                    grid[x][y] = grid[x][y - 1];
+                    grid[x][y - 1] = null;
                     y--;
                 }
                 console.log('left!');
                 break;
             }
-            case arrows.right: {
-                if(y<this.state.size - 1) {
-                    grid[x][y] = grid[x][y+1];
-                    grid[x][y+1] = null;
+            case 'right': {
+                if (y < this.state.size - 1) {
+                    grid[x][y] = grid[x][y + 1];
+                    grid[x][y + 1] = null;
                     y++;
                 }
                 console.log('right!');
                 break;
             }
         }
-
         this.setState({
             grid,
             emptyPos: {
@@ -109,14 +123,40 @@ class App extends Component {
         })
     };
 
+
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    reshuffleTiles() {
+        for (let i = 1; i <= 10; i++) {
+            switch (this.getRandomInt(1, 4)) {
+                case 1:
+                    this.moveTile('up');
+                    break;
+                case 2:
+                    this.moveTile('down');
+                    break;
+                case 3:
+                    this.moveTile('left');
+                    break;
+                case 4:
+                    this.moveTile('right');
+                    break;
+            }
+
+            console.log(this.state.grid, this.state.emptyPos)
+        }
+    };
+
     render() {
         let tiles = [];
         this.state.grid.forEach((row) => {
             tiles = tiles.concat(row);
         });
         tiles = tiles.map((tile, index) => (
-            <div key={index} className={tile?"Grid__Tile":"Grid__Tile Grid__Tile_empty"}>
-                {tile?tile:''}
+            <div key={index} className={tile ? "Grid__Tile" : "Grid__Tile Grid__Tile_empty"}>
+                {tile ? tile : ''}
             </div>
         ));
 
@@ -126,13 +166,17 @@ class App extends Component {
                 <br/>
                 <label>
                     <span>5 на 5</span>
-                    <input name="size" type="radio" checked={this.state.size===5}/>
+                    <input name="size" type="radio" checked={this.state.size === 5}/>
                 </label>
 
                 <label>
                     <span>6 на 6</span>
-                    <input name="size" type="radio" checked={this.state.size===6}/>
+                    <input name="size" type="radio" checked={this.state.size === 6}/>
                 </label>
+
+                <div>
+                    <button type="button" onClick={()=>this.reshuffleTiles()}>Нова гра</button>
+                </div>
 
                 <div className="Grid">
                     {tiles}
